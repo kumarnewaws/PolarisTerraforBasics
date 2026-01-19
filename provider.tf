d4210de64713e9d5cb618e745c4754f760b097cf
chaitanya <chaitanya@gmail.com>
2026-01-19 19:47:46 +0530

    backend updated in terraform as s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "polaris-backend"
+    key    = "polarisbasics/basics.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
+
 provider "aws" {
   region = "ap-south-1"
 
